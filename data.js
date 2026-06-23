688a62447fdbeca77b48584ebb34ebbee5113b97
lexxdaniloff <55055797+lexdanilov@users.noreply.github.com>
2026-06-23 21:12:36 +0300

    upload

---
+++ data.js
@@ -0,0 +1,311 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Скульптура Льва",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": -0.05257120265897797,
+        "pitch": 0.010508002705510222,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5727101510910302,
+          "pitch": -0.005419212692171271,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 0.4908949492930894,
+          "pitch": 0.07805628915705753,
+          "rotation": 0,
+          "target": "4-----"
+        },
+        {
+          "yaw": 1.1214800737524335,
+          "pitch": 0.019649983237115975,
+          "rotation": 0,
+          "target": "3---"
+        },
+        {
+          "yaw": -0.8214506669077721,
+          "pitch": -0.025981223780014773,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Скульптура Оленя",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0.20219550085518811,
+        "pitch": -0.04503482766635081,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6618442381992935,
+          "pitch": -0.07964089333846758,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": -1.2851964460854308,
+          "pitch": 0.01599218897536403,
+          "rotation": 0,
+          "target": "4-----"
+        },
+        {
+          "yaw": -1.0878955346067904,
+          "pitch": -0.021607461986780763,
+          "rotation": 0,
+          "target": "3---"
+        },
+        {
+          "yaw": 2.253509202394252,
+          "pitch": -0.009673629409714835,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Скульптура Барса",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.254921114586983,
+          "pitch": -0.0198573906770676,
+          "rotation": 0,
+          "target": "3---"
+        },
+        {
+          "yaw": 1.3853819123530062,
+          "pitch": -0.027800239808243532,
+          "rotation": 0,
+          "target": "4-----"
+        },
+        {
+          "yaw": 1.5299700787741752,
+          "pitch": -0.058901689285377756,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": 1.6799679733169182,
+          "pitch": -0.06299112031703302,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---",
+      "name": "Скульптура Квантового Компьютера",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.998754791381371,
+          "pitch": -0.002018749655334773,
+          "rotation": 0,
+          "target": "4-----"
+        },
+        {
+          "yaw": 1.3701931321075183,
+          "pitch": -0.14374421528572512,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": 2.1952957817970518,
+          "pitch": -0.06308355968723944,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": 2.5651398781536994,
+          "pitch": -0.021202924586466665,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-----",
+      "name": "Скульптура Квантового Компьютера (временный образец)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": -0.177107459062114,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3600027998140956,
+          "pitch": 0.00815096181370123,
+          "rotation": 0,
+          "target": "3---"
+        },
+        {
+          "yaw": -2.9392405652337725,
+          "pitch": -0.2820697556679139,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": -1.5030704566642896,
+          "pitch": -0.08226663278668056,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -1.0122402747652561,
+          "pitch": -0.015314142881745596,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
